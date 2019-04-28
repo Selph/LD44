50cbe1479cb18eb37ab5583431fbb36f6294e54b
fhamel <fhamel@gmail.com>
2019-04-28 11:12:53 -0400

    Add a few logs

--- Assets/GoogleARCore/Examples/CloudAnchors/Scripts/LocalPlayerController.cs
+++ Assets/GoogleARCore/Examples/CloudAnchors/Scripts/LocalPlayerController.cs
@@ -94,6 +94,7 @@ namespace GoogleARCore.Examples.CloudAnchors
                 // Spawn the object in all clients.
                 NetworkServer.Spawn(starObject);
                 StarsToPlace--;
+                Debug.LogFormat("Server: {0} stars left to place for id {1}", StarsToPlace, netId);
 
                 if (StarsToPlace == 0 && OnAllStarsPlaced != null)
                 {
@@ -189,6 +190,7 @@ namespace GoogleARCore.Examples.CloudAnchors
                 var gameState = FindObjectOfType<GameState>();
                 if (gameState)
                 {
+                    Debug.Log("All stars placed!");
                     gameState.SetGameMode(GameState.GameMode.Playing);
                 }
                 else
